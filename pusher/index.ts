b8bd54c64d44e07aeb1c609614d4099286d8d9ef
PraTEEkgUpTa2 <97375784+PraTEEkgUpTa2@users.noreply.github.com>
2025-09-07 01:40:42 +0530

    final

--- pusher/index.ts
+++ pusher/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import { createClient } from 'redis';
+
+async function main() {
+    const client = createClient();
+    client.on('error', (err)=>{
+        console.error('Redis Client Error', err);
+    })
+    .connect();
+
+    const res = await client.xAdd('betteruptime:website', '*', {
+        id: "1",
+        url: "https://google.com"
+    })
+
+    console.log(res);
+
+    client.destroy(); // to destroy thr connection
+}
+
+main();
--- worker/index.ts
+++ worker/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import { createClient } from 'redis';
+
+async function main() {
+    const client = await createClient()
+    .on('error', (err)=>{
+        console.error('Redis Client Error', err);
+    }).connect();
+
+    const res = await client.xReadGroup('india','india-1',{
+        key: 'betteruptime:website',
+        id: '>'
+    }, {
+        COUNT: 2,
+    })
+    
+    //@ts-ignore
+    console.log(res[0].messages);
+
+    client.destroy(); // to destroy thr connection
+}
+
+main();
